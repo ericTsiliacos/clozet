7af3c67700f0582e80155e37476dd6135a3be156
Eric Tsiliacos <erictsiliacos@gmail.com>
2016-03-26 15:39:35 -0700

    Run all tests including api tests

--- Rakefile
+++ Rakefile
@@ -8,6 +8,8 @@ task :tests do
     puts `pwd`
     puts 'compiling backend...'
     system 'stack build'
+    system 'stack test'
+
     pid = spawn("PORT=8080 .stack-work/install/x86_64-osx/lts-4.1/7.10.3/bin/clozet-exe", :out => "../spec/logs/server.out", :err => "../spec/logs/server.err")
     Process.detach(pid)
 
--- backend/test/Spec.hs
+++ backend/test/Spec.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 module Main (main) where
 
-import           Test.Hspec
-import           Test.Hspec.Wai
-
-import           Example (app)
+import Test.Hspec
+import Test.Hspec.Wai
+import Server (app)
 
 main :: IO ()
 main = hspec spec
@@ -12,6 +11,6 @@ main = hspec spec
 spec :: Spec
 spec = with app $ do
   describe "GET /" $ do
-    it "responds with 200 / 'hello'" $ do
-      get "/" `shouldRespondWith` "hello world" {matchStatus = 200}
+    it "responds with 200 'hello'" $ do
+      get "/hello" `shouldRespondWith` "hello" {matchStatus = 200}
 
